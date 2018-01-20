5fc952415a97c46c0e2f8cbb0c6c3e80981d68c6
Arcanitor <damachinator@gmail.com>
2018-01-20 11:27:29 -0500

    chat -> MC bridge

---
+++ src/main/java/arcanitor/civilengineering/server/ServerChatWriter.java
@@ -0,0 +1,36 @@
+package arcanitor.civilengineering.server;
+
+import arcanitor.civilengineering.CivilEngineering;
+import arcanitor.civilengineering.bridge.ApiMessage;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ServerChatWriter {
+    public static ConcurrentLinkedQueue<ApiMessage> messages = new ConcurrentLinkedQueue();
+
+    @SubscribeEvent
+    public static void onServerUpdate(TickEvent.ServerTickEvent event) {
+        ApiMessage nextMessage = messages.poll();
+
+        if (nextMessage != null) {
+            CivilEngineering.logger.info("Processing message");
+            String user = nextMessage.getUsername();
+            String text = nextMessage.getMessage().trim();
+
+            String message;
+
+            if (!text.isEmpty()) {
+                if (nextMessage.getEvent() == "user_action") {
+                    message = "* " + user + " " + text;
+                } else {
+                    message = "<" + user + "> " + text;
+                }
+                FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().sendMessage(new TextComponentString(message));
+            }
+        }
+    }
+}
