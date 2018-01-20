12778e855d2270704fff902c099ab5a9dc6f0d55
DaMachinator <damachinator@gmail.com>
2018-01-19 19:52:19 -0500

    Fix structure

---
+++ src/main/java/arcanitor/civilengineering/eventhandlers/HandleChatMessage.java
@@ -0,0 +1,14 @@
+package arcanitor.civilengineering.eventhandlers;
+
+import arcanitor.civilengineering.CivilEngineering;
+import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber
+public class HandleChatMessage {
+    @SubscribeEvent
+    public static void chatMessageGlobal(ServerChatEvent event) {
+        CivilEngineering.logger.info("Message on server: "+event.getMessage()+" sent by "+event.getUsername());
+    }
+}
