2880f6c7a5a19fc78b0bf17426506356a8d4c9ca
DaMachinator <damachinator@gmail.com>
2018-01-23 13:04:10 -0500

    Add join/leave handler

---
+++ src/main/kotlin/civilengineering/eventhandlers/JoinLeaveHandler.kt
@@ -0,0 +1,32 @@
+package civilengineering.eventhandlers
+
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.PlayerEvent
+import civilengineering.Util.antiping
+import civilengineering.bridge.ApiMessage
+import civilengineering.bridge.MessageHandler
+import civilengineering.cfg
+
+class JoinLeaveHandler {
+    @SubscribeEvent
+    fun handleJoinEvent(event: PlayerEvent.PlayerLoggedInEvent) {
+        if(cfg!!.relay.joinLeave) {
+            val player:String = event.player.name.antiping()
+            MessageHandler.transmit(ApiMessage(
+                    username = "Server",
+                    text = "$player has connected to the server."
+            ))
+        }
+    }
+
+    @SubscribeEvent
+    fun handleLeaveEvent(event: PlayerEvent.PlayerLoggedOutEvent) {
+        if(cfg!!.relay.joinLeave) {
+            val player:String = event.player.name.antiping()
+            MessageHandler.transmit(ApiMessage(
+                    username = "Server",
+                    text = "$player has disconnected from the server."
+            ))
+        }
+    }
+}
