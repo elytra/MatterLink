39d6f0905778d6b508dc91f828cf8dd4e0ffb8df
DaMachinator <damachinator@gmail.com>
2018-01-23 10:30:54 -0500

    Implement gateway check on incoming messages. Fixes issue #4

--- src/main/kotlin/civilengineering/bridge/ServerChatHelper.kt
+++ src/main/kotlin/civilengineering/bridge/ServerChatHelper.kt
@@ -1,6 +1,7 @@
 package civilengineering.bridge
 
 import civilengineering.CivilEngineering
+import civilengineering.cfg
 import net.minecraft.util.text.TextComponentString
 import net.minecraftforge.fml.common.FMLCommonHandler
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
@@ -13,7 +14,7 @@ class ServerChatHelper {
             CivilEngineering.logger.debug("incoming: " + MessageHandler.rcvQueue.toString())
         val nextMessage = MessageHandler.rcvQueue.poll()
 
-        if (nextMessage != null) {
+        if (nextMessage != null && nextMessage.gateway == cfg!!.connect.gateway) {
             val user = nextMessage.username
             val text = nextMessage.text.trim()
 
